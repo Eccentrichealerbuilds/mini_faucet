f39cfbd6571587cefd566dbaa12c1080a8581852
Eccentrichealerbuilds <healercpp@gmail.com>
2026-03-09 11:28:58 +0100

    finish with designing the claim page

--- faucet-frontend/src/components/claim_input.rs
+++ faucet-frontend/src/components/claim_input.rs
@@ -2,17 +2,22 @@ use dioxus::prelude::*;
 const WALLET_SUBMIT_CSS : Asset = asset!("assets/tailwind.css");
 
 pub fn claim_input() -> Element  {
-    rsx!{ document::Stylesheet {href: WALLET_SUBMIT_CSS}
-        div {class: "bg-black text-white min-h-screen p-4",
-            nav {class: "flex items-center justify-between p-6  bg-neutral-950 text-white",  }
-            div { class: "bg-neutral-900 rounded-3xl p-12 shadow-2xl w-full max-w-xl mx-auto mt-60",
-                h1 { class: "text-2xl font-extrabold text-center", "WELCOME MINI SEPOLIA FAUCET" }
-                p { class: "text-xl text-neutral-400 mb-10 text-center mt-1" , "claim your daily sepolia faucet here" }
-                label {class: "block text-lg mb-3 text-neutral-300", "Enter your Address here" }
-                input {class: "w-full p-4 rounded-xl border border-neutral-700 bg-neutral-800 text-white placeholder-neutral-500", placeholder:"0x000000000000000000", }
-                button {class: "w-full mt-6 p-4 rounded-xl bg-blue-600 text-white font-extrabold font-stretch-200% hover:bg-blue-700 transition", "Submit Wallet"} 
-            } 
-            footer { class:"","footer" }
+    rsx!{ document::Stylesheet { href: WALLET_SUBMIT_CSS},
+        div {class: "bg-gray-950 text-white min-h-screen bg-blend-overlay",
+            nav { class: "bg-gray-400 p-6", }
+            div{class: "bg-slate-500 mx-auto mt-60 max-w-2xl p-12 mask-t-from-0.2
+                rounded-3xl h-full mask-t-from-0.5",
+                h1 {class: "text-center text-2xl font-extrabold
+                    font-stretch-200%","WELCOME TO SEPOLIA MINI FAUCET" }br {  }
+                label {class:" after:content-['*'] after:text-red-500 
+                    after:ml-1
+                    text-lg text-neutral-400 font-bold", "Enter your Address below" }br {}
+                input { class:"border-4 border-dotted border-blue-700 w-full
+                    p-4 mb-4 rounded-xl",placeholder:"0x07ac47d64cb44cd0c7d7557be5fc02b530bd2d10" }br{}
+                button { class:" p-4 rounded-xl bg-indigo-700 w-full
+                    hover:bg-red-400 hover:duration-500 transition duration-1000 ", "Submit Wallet"}
+            }
         }
+
     }
 }
